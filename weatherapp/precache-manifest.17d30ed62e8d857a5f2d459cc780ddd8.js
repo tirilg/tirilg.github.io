ec1aea5e3abbd0dd9d7c41e2cfb8c270e3ca0fc7
Tiril Grimsrud <tiril_k_g@hotmail.com>
2020-04-27 10:20:10 +0200

    auto-commit

---
+++ weatherapp/precache-manifest.17d30ed62e8d857a5f2d459cc780ddd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee9bee36978bb573b68f7cd117edcf2",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "457bae78676626fd2ec1",
+    "url": "/weatherapp/static/css/main.40058e0b.chunk.css"
+  },
+  {
+    "revision": "5eb7a2b40110615d9dbb",
+    "url": "/weatherapp/static/js/2.d40e4d5c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weatherapp/static/js/2.d40e4d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457bae78676626fd2ec1",
+    "url": "/weatherapp/static/js/main.0cf30d5f.chunk.js"
+  },
+  {
+    "revision": "ff873d0a6dca386d57e6",
+    "url": "/weatherapp/static/js/runtime-main.d6b406ef.js"
+  },
+  {
+    "revision": "6f381937902dec4c3fc2eaebc3a4f640",
+    "url": "/weatherapp/static/media/background.6f381937.svg"
+  }
+]);
